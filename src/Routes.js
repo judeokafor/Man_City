2b6c8a4baa75ede47a8c9ccaa2971699b97c4587
Okafor Jude <Okaforjudechukwuebuka@gmail.com>
2019-04-09 05:57:55 +0100

    add header and footer

---
+++ src/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from './hoc/Layout';
+
+const Routes = props => {
+   return (
+      <div className='App'>
+         <Layout>Second line</Layout>
+      </div>
+   );
+};
+
+export default Routes;
