df4dc218503f7a2da3fd0a1b59e571530f6074d0
judeokafor <okaforjudechukwuebuka@gmail.com>
2019-04-10 18:40:39 +0100

    authentication complete

--- src/Routes.js
+++ src/Routes.js
@@ -4,14 +4,38 @@ import { Switch, Route } from "react-router-dom";
 import Home from "./components/home";
 import SignIn from "./components/sign_in";
 import Dashboard from "./components/admin/Dashboard";
+
+import PrivateRoutes from "./components/authRoutes/PrivateRoutes";
+import PublicRoutes from "./components/authRoutes/PublicRoutes";
 const Routes = props => {
+  console.log(props);
   return (
     <div className="App">
       <Layout>
         <Switch>
-          <Route exact component={Dashboard} path="/dashboard" />
-          <Route exact component={SignIn} path="/sign_in" />
-          <Route exact component={Home} path="/" />
+          <PrivateRoutes
+            {...props}
+            path="/dashboard"
+            exact
+            component={Dashboard}
+          />
+          <PublicRoutes
+            restricted={false}
+            {...props}
+            path="/"
+            exact
+            component={Home}
+          />
+          <PublicRoutes
+            restricted={true}
+            {...props}
+            path="/sign_in"
+            exact
+            component={SignIn}
+          />
+
+          {/* <Route exact component={SignIn} path="/sign_in" />
+          <Route exact component={Home} path="/" /> */}
         </Switch>
       </Layout>
     </div>
